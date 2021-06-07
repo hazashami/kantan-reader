14f42494b4b65817a5d402949bbd1bbacf14c092
Haza Shami <hazashami@gmail.com>
2021-06-06 22:38:45 -0700

    added input handling and auth skeleton

--- src/components/AuthPanel.jsx
+++ src/components/AuthPanel.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useContext } from 'react';
+
+import AppContext from '../context/AppContext';
 
 const AuthPanel = () => {
+    const { axiosInstance } = useContext(AppContext);
+
+    const [ loginInfo, setLoginInfo ] = useState({
+        "username": '',
+        "password": ''
+    });
+    const [ bearer, setBearer ] = useState('');
+
+    const handleInputChange = (event) => {
+        if (event.target instanceof HTMLInputElement) {
+            console.log(event.target.name);
+            console.log(event.target.value);
+            setLoginInfo({...loginInfo, [event.target.name]: event.target.value});
+        }
+    }
+
+    const login = () => {
+        axios.get(host + "/auth/login", loginInfo)
+        .then((response) => {
+            console.log(response);
+            setBearer(response.token.session);
+        })
+        .catch((err) => {
+            console.err(err);
+        });
+    }
 
     return (
-        <div>
-            AuthPanel
-        </div>
+        <form className="authPanelForm">
+            <label className="authLabel">Username:</label>
+            <input name="username" className="authInput" type="text" value={loginInfo["username"]} onChange={handleInputChange} />
+            <label className="authLabel">Password:</label>
+            <input name="password" className="authInput" type="password" value={loginInfo["password"]} onChange={handleInputChange} />
+            <label className="authLabelBearer">Bearer Auth:</label>
+            <input name="bearer" className="authInput" type="text" value={bearer} 
+                readOnly={true} placeholder="read only" onChange={handleInputChange} />
+        </form>
     )
 }
 
